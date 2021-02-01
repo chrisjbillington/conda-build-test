8f60cf89d0bb52535e146d807ee9c48bda532f64
chrisjbillington <chrisjbillington@gmail.com>
2021-02-01 16:29:24 +1100

    printlines

--- build_conda_package.bat
+++ build_conda_package.bat
@@ -1,7 +1,14 @@
+echo "setting env var"
 SET CONDA_INSTALLER=Miniconda3-latest-Windows-x86_64.exe
+echo "downloading conda"
 curl -LO https://repo.continuum.io/miniconda/%CONDA_INSTALLER%
+echo "installing conda"
 %CONDA_INSTALLER% /S /D=%CD%\.miniconda
+echo "activating"
 .miniconda\Scripts\activate
+echo "updating conda base"
 conda update -n base -c defaults conda
+echo "installing conda-build"
 conda install conda-build
+echo "building"
 conda-build .
