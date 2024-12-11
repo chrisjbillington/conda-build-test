98485f4bd5b616f601017f9cd66fa7ccb0714486
chrisjbillington <chrisjbillington@gmail.com>
2024-12-11 11:04:28 +1030

    Temporarily make package pure for faster testing

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import os
 from setuptools import setup, Extension
 
 EXT_MODULES = [
-    Extension(
-        'hello.hello',
-        sources=[os.path.join('src', 'hello.c')],
-    )
+    # Extension(
+    #     'hello.hello',
+    #     sources=[os.path.join('src', 'hello.c')],
+    # )
 ]
 
 setup(ext_modules=EXT_MODULES)
